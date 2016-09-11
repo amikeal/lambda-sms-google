f953101ec2ff4da06dc47e315bab9ecd8a9a8d32
Adam Mikeal <adam@mikeal.org>
2016-09-11 11:07:36 -0700

    Added logging for debug purposes

--- lambda_function.py
+++ lambda_function.py
@@ -5,8 +5,9 @@ from time import strftime as timestamp
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
-# Set a logging level
+# Set a logging level, include timestamp
 LOG_LEVEL = log.DEBUG
+log.basicConfig(level=LOG_LEVEL, format='%(asctime)s %(message)s')
 
 # Setup access to Google sheets
 scopes = ['https://spreadsheets.google.com/feeds']
@@ -18,30 +19,30 @@ def log(msg):
 def addrow(sender, location, text):
     gc = gspread.authorize(credentials)
     sheet = gc.open_by_key('1bGpMTkkInMjrupVQouydb2WZDH7c2jeIOtlcYbgOC6g')
-    log("Opening default worksheet 'INBOX'")
+    log.info("Opening default worksheet 'INBOX'")
     worksheet = sheet.worksheet("INBOX")
     # Check to see if a worksheet for today's date exists; if not, create it
-    log("Fetching list of worksheets...")
+    log.info("Fetching list of worksheets...")
     worksheet_list = sheet.worksheets()
     worksheet_names = [w.title for w in worksheet_list]
     date_name = timestamp('%Y-%m-%d')
-    log("Checking for existing worksheet with title: %s" % (date_name))
+    log.info("Checking for existing worksheet with title: %s" % (date_name))
     if date_name not in worksheet_names:
-        log("Named worksheet not found... creating worksheet")
+        log.info("Named worksheet not found... creating worksheet")
         worksheet = sheet.add_worksheet(title=date_name, rows="1", cols="20")
     else:
-        log("Found workshet... opening worksheet for writing")
+        log.info("Found workshet... opening worksheet for writing")
         worksheet = sheet.worksheet(date_name)
     # Insert the data into the opened worksheet
     field_list = [timestamp('%Y-%m-%d %H:%M:%S'), sender, location] + [x.strip() for x in text.split(',')]
-    log("Appending new row to worksheet")
+    log.info("Appending new row to worksheet (calling gspread.append_row()... )")
     worksheet.append_row(field_list)
-    log("Appended: %s to Worksheet named %s" % (field_list, worksheet.title))
+    log.info("Appended: %s to Worksheet named %s" % (field_list, worksheet.title))
     return True
 
 def lambda_handler(event, context):
     # turn off logging once things are up and running
-    log("Received event: " + json.dumps(event, indent=2))
+    log.info("Received event: " + json.dumps(event, indent=2))
     sender = event["fromNumber"]
     location = event["fromLocation"]
     msg_body = event["body"]
